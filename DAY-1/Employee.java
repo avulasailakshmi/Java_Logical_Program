70a7342c7b10c06535ecb5dabfc3a9981892c8a8
avulasailakshmi <avulasailakshmi4@gmail.com>
2024-11-07 16:16:32 +0530

    Add files via upload

---
+++ DAY-1/Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+public static void main(String args[])
+{
+int eno=101;
+String ename="Karun";
+float ft=544.45F;
+System.out.println("the given Employee number is:"+eno);
+System.out.println("the given Employee name is:"+ename);
+System.out.println("the given Employee salary is:"+ft);
+}
+}
---
+++ DAY-1/First.java
@@ -0,0 +1,9 @@
+//sample program
+import java.lang.*;
+class First
+{
+public static void main(String args[])
+{
+System.out.print("This is my first program");
+}
+}
